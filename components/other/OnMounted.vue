b875335921bd831d8bc856906ac19bf5d02150d2
fredcallaway <fredc@llaway.com>
2025-12-07 17:18:00 -0500

    add delay prop to OnMounted

--- components/other/OnMounted.vue
+++ components/other/OnMounted.vue
@@ -3,14 +3,16 @@ const onceCache = new Set<string>()
 </script>
 
 <script lang="ts" setup>
+const { sleep } = useLocalAsync()
 
 const props = defineProps<{
   fn: () => void
   once?: boolean | string
+  delay?: number
 }>()
 
 
-onMounted(() => {
+onMounted(async () => {
   if (props.once) {
     const key = typeof props.once === 'string' ? props.once : props.fn.toString()
     if (onceCache.has(key)) {
@@ -18,12 +20,13 @@ onMounted(() => {
     }
     onceCache.add(key)
   }
+  if (props.delay) {
+    await sleep(props.delay)
+  }
   props.fn()
 })
+
 </script>
 
 <template>
-  <div>
-    
-  </div>
 </template>
