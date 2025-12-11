b372ba2199fba48ff7015595d2375f654de07622
fredcallaway <fredc@llaway.com>
2025-12-11 17:01:01 -0500

    Key

---
+++ components/other/Key.vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup>
+
+
+</script>
+
+<template>
+  <kbd inline-block px-2 py-1 mx-0.5 rounded border border-gray-300 bg-gray-50 font-mono text-sm>
+    <slot />
+  </kbd>
+</template>
