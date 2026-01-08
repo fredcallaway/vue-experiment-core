8f235764533c05faa3b052e417ffea96060393bf
fredcallaway <fredc@llaway.com>
2026-01-08 11:12:46 -0500

    BREAKING ENoReturn no margin

--- components/epochs/ENoReturn.vue
+++ components/epochs/ENoReturn.vue
@@ -10,15 +10,13 @@ const confirm = () => {
 </script>
 
 <template>
-  <div w-130 mx-auto mt-30 flex-center gap-10>
-    <div card-yellow >
-      <h3>Warning!</h3>
+  <div w-135 card-yellow flex-center text-center mx-auto text-lg>
+    <h3>Warning!</h3>
 
-      Once you continue past this page, you cannot restart the study.
-      <br/>
-      If you refresh the page or close the browser window, you will not be able to complete the study!
+    Once you continue past this page, you cannot restart the study.
+    <br/>
+    If you refresh the page or close the browser window, you will not be able to complete the study!
 
-    </div>
-    <PButton value="I will not refresh the page" @click="confirm" />
+    <PButton value="I will not refresh the page" @click="confirm" mx-auto btn-red bg-opacity-80 mt-5 />
   </div>
 </template>
