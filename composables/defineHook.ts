3932e442cd7bc09393d60f6aa7f1fbaccf9f9d9f
fredcallaway <fredc@llaway.com>
2026-01-08 11:13:01 -0500

    defineHook: add on(callback)

--- composables/defineHook.ts
+++ composables/defineHook.ts
@@ -3,10 +3,12 @@ type HookCallback<S> = (state: S) => void | Promise<void>
 type Hook<S> = {
   emit: (state: S) => Promise<void>
   receive: (callback?: HookCallback<S>) => Promise<void>
+  on: (callback: HookCallback<S>) => () => void // unsubscribe
 }
 
 export function defineHook<S>(): Hook<S> {
   const queue: Array<{ resolve: () => void; callback: HookCallback<S> }> = []
+  const subscribers: Set<HookCallback<S>> = new Set()
 
   return {
     async emit(state) {
@@ -15,12 +17,22 @@ export function defineHook<S>(): Hook<S> {
         await waiter.callback(state)
         waiter.resolve()
       }
+      for (const callback of subscribers) {
+        await callback(state)
+      }
     },
 
     receive(callback = () => {}) {
       return new Promise((resolve) => {
         queue.push({ resolve, callback })
       })
+    },
+
+    on(callback) {
+      subscribers.add(callback)
+      return () => {
+        subscribers.delete(callback)
+      }
     },
   }
 }
