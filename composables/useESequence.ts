c643d7588bd7a66f1dd5bcef0738ea6ba88fa49b
fredcallaway <fredc@llaway.com>
2026-01-11 13:06:41 -0500

    create useESequence

---
+++ composables/useESequence.ts
@@ -0,0 +1,19 @@
+import { defineComponent, h, type SetupContext, type SlotsType } from 'vue'
+import { useIndexableEpoch, type IndexableEpoch } from './useEpoch'
+import ESequence from '../components/epochs/ESequence.vue'
+
+export function useESequence(name: string) {
+  const E = useIndexableEpoch(name, 0)
+  
+  const Sequence = defineComponent({
+    name: 'Sequence',
+    slots: Object as SlotsType<{ default: () => any }>,
+    setup(_props, { slots }: SetupContext) {
+      return () => {
+        return h(ESequence, { epoch: E }, slots.default?.())
+      }
+    },
+  })
+
+  return {E, Sequence}
+}
