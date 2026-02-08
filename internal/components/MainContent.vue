16e68ecf683c7c3d0e2bbe14c0cc95f4cf7da44d
fredcallaway <fredc@llaway.com>
2026-02-08 13:50:58 -0500

    remove error handler

--- internal/components/MainContent.vue
+++ internal/components/MainContent.vue
@@ -29,37 +29,38 @@ whenever(content, (el) => {
 const error = ref(false)
 
 const nuxtApp = useNuxtApp()
-const defaultHandler = nuxtApp.vueApp.config.errorHandler
-nuxtApp.vueApp.config.errorHandler = (err, instance, info) => {
 
-  // this error is expected behavior; see useLocalAsync.ts
-  if (err === 'useLocalAsync:unmounted') {
-    // console.debug('caught useLocalAsync:unmounted')
-    return
-  }
+// const defaultHandler = nuxtApp.vueApp.config.errorHandler
+// nuxtApp.vueApp.config.errorHandler = (err, instance, info) => {
 
-  if (props.captureErrors) {
-    const componentName = instance?.$options?.__name
-    const componentPath = instance?.$options?.__file
-    logError(err as Error, {info, componentName, componentPath})
-    useCurrentSession().error = String(err)
-    error.value = true
-    return
-  }
+//   // this error is expected behavior; see useLocalAsync.ts
+//   if (err === 'useLocalAsync:unmounted') {
+//     // console.debug('caught useLocalAsync:unmounted')
+//     return
+//   }
+
+//   if (props.captureErrors) {
+//     const componentName = instance?.$options?.__name
+//     const componentPath = instance?.$options?.__file
+//     logError(err as Error, {info, componentName, componentPath})
+//     useCurrentSession().error = String(err)
+//     error.value = true
+//     return
+//   }
   
-  if (defaultHandler) {
-    console.log('calling default handler')
-    // TODO don't use default handler so that the developer interface is maintained
-    // could add devtools navigation here
-    // const devtoolsClient = useNuxtDevTools()
-    // devtoolsClient.value?.devtools.navigate('/modules/error')
-    defaultHandler(err, instance, info)
-  } else {
-    // I don't think this case is possible, but just in case
-    console.error('No default error handler found')
-    throw new Error('No default error handler found')
-  }
-}
+//   if (defaultHandler) {
+//     console.log('calling default handler')
+//     // TODO don't use default handler so that the developer interface is maintained
+//     // could add devtools navigation here
+//     // const devtoolsClient = useNuxtDevTools()
+//     // devtoolsClient.value?.devtools.navigate('/modules/error')
+//     defaultHandler(err, instance, info)
+//   } else {
+//     // I don't think this case is possible, but just in case
+//     console.error('No default error handler found')
+//     throw new Error('No default error handler found')
+//   }
+// }
 
 </script>
 
