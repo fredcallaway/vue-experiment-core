60b5e6e456bcf0762d94b39c585bcac5e576b526
fredcallaway <fredc@llaway.com>
2026-02-10 09:29:44 -0500

    MainContentModal (WIP)

---
+++ components/other/MainContentModal.vue
@@ -0,0 +1,9 @@
+<template>
+  <MainContentOverlay>
+    <div class="bg-black/30 inset-0 h-full w-full flex-center z-100 relative">
+      <div bg-white p-4 rounded-lg shadow-xl max-w-md mb-30 z-105>
+        <slot />
+      </div>
+    </div>
+  </MainContentOverlay>
+</template>
