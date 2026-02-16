e818bec33265904a81a1edca00124e1ade37a5dc
fredcallaway <fredc@llaway.com>
2026-02-15 22:25:45 -0500

    add useErrorHandler

---
+++ composables/useErrorHandler.ts
@@ -0,0 +1,78 @@
+type ErrorHandler = (
+  err: unknown,
+  instance: any,
+  info: string,
+  next: () => void
+) => void
+
+type HandlerEntry = {
+  handler: ErrorHandler
+  priority: number
+  sequence: number
+}
+
+export const useErrorHandler = createGlobalState(() => {
+  const nuxtApp = useNuxtApp()
+  const originalHandler = nuxtApp.vueApp.config.errorHandler
+  
+  const handlers = ref<HandlerEntry[]>([])
+  const globalIgnoreErrors = ref<Set<string>>(new Set(['useLocalAsync:unmounted']))
+  let sequenceCounter = 0
+
+  const getSortedHandlers = () => {
+    return R.pipe(
+      handlers.value,
+      R.sortBy((entry) => -entry.priority, (entry) => -entry.sequence),
+    ).map(e => e.handler)
+  }
+
+  const vueErrorHandler = (err: unknown, instance: any, info: string) => {
+    const errorString = String(err)
+    
+    if (globalIgnoreErrors.value.has(errorString)) {
+      return
+    }
+
+    const sortedHandlers = getSortedHandlers()
+    let currentIndex = 0
+    
+    const next = () => {
+      currentIndex++
+      if (currentIndex < sortedHandlers.length) {
+        sortedHandlers[currentIndex](err, instance, info, next)
+      } else {
+        if (originalHandler) {
+          originalHandler(err, instance, info)
+        } else {
+          console.error('Unhandled error:', err)
+        }
+      }
+    }
+
+    if (sortedHandlers.length > 0) {
+      sortedHandlers[0](err, instance, info, next)
+    } else {
+      next()
+    }
+  }
+
+  nuxtApp.vueApp.config.errorHandler = vueErrorHandler
+
+  const pushHandler = (handler: ErrorHandler, priority = 0) => {
+    handlers.value.push({ handler, priority, sequence: sequenceCounter++ })
+    return () => popHandler(handler)
+  }
+
+  const popHandler = (handler: ErrorHandler) => {
+    const index = handlers.value.findIndex(e => e.handler === handler)
+    if (index !== -1) {
+      handlers.value.splice(index, 1)
+    }
+  }
+
+  return {
+    pushHandler,
+    popHandler,
+    globalIgnoreErrors,
+  }
+})
